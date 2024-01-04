4a4244f7b9d0b9cf1211d5d6f17a9f06b6155e55
Kimjh laptop <kim_jh6859@naver.com>
2024-01-04 18:44:52 +0900

    메인페이지 헤더

--- front/src/container/header/Header.js
+++ front/src/container/header/Header.js
@@ -20,11 +20,35 @@ function Header() {
 
     return (
         <div className={styles.container}>
-            <div className={`${isHidden ? styles.hiddenHeader : styles.header1}`}>
-                header1
+            <div className={`${styles.header1} ${isHidden ? styles.hiddenHeader1 : ''}`}>
+                <div className={styles.buttonBox}>
+                    <div className={styles.buttons}>
+                        <div className={styles.button}>
+                            아직안넣음
+                        </div>
+                    </div>
+                    <div className={styles.buttons}>
+                        <div className={styles.button}>
+                            로그인/회원가입
+                        </div>
+                        <div className={styles.button}>
+                            마이페이지
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div className={styles.header2}>
-                header2
+            <div className={`${styles.header2} ${isHidden ? styles.hiddenHeader2 : ''}`}>
+                <div className={styles.first}>
+                    <div className={styles.logo}>
+                        Logo
+                    </div>
+                    <div className={styles.search}>
+                        <input size={'100%'} className={styles.input}/>
+                    </div>
+                    <div className={styles.buttons2}>
+                        d
+                    </div>
+                </div>
             </div>
         </div>
     );
--- front/src/container/main/ImageBox.js
+++ front/src/container/main/ImageBox.js
@@ -1,9 +1,27 @@
 import styles from "./ImageBox.module.css"
+import { FaAngleLeft, FaAngleRight } from "react-icons/fa";
 
 function ImageBox() {
     return (  
         <div className={styles.container}>
-            image
+            <div className={styles.buttons}>
+                <FaAngleLeft size={30} color="#aaaaaa" cursor={"pointer"}/>
+                <FaAngleRight size={30} color="#aaaaaa" cursor={"pointer"}/>
+            </div>
+            <div className={styles.boxes}>
+                <div className={styles.box}>
+                    s
+                </div>
+                <div className={styles.box}>
+s
+                </div>
+                <div className={styles.box}>
+s
+                </div>
+                <div className={styles.box}>
+s
+                </div>
+            </div>
         </div>
     );
 }
