aed4d7c4ec3cf0ba76a2c72012c6fafe6dcb474d
Kimjh laptop <kim_jh6859@naver.com>
2024-01-04 17:08:29 +0900

    fixed 해제

--- front/src/index.js
+++ front/src/index.js
@@ -3,12 +3,18 @@ import * as ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import Main from "./routes/main/Main";
+import Login from "./routes/login/Login";
 import ErrorPage from "./routes/ErrorPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Main />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
     errorElement: <ErrorPage />,
   },
 ]);
