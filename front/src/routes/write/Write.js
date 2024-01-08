5cf2537668e2eb04ec0dd1929e7f8981fdb84b13
kyuchory <sls789456@naver.com>
2024-01-08 19:14:27 +0900

    글쓰기 front

--- front/src/routes/write/Write.js
+++ front/src/routes/write/Write.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import Header from "../../container/header/Header";
 import styles from "./Write.module.css";
 import addImage from "./icon/addImage.png";
@@ -67,6 +67,52 @@ function Write() {
       setState3(true);
     }
   };
+  const [direct, setDirect] = useState(false);
+  const [parcel, setParcel] = useState(false);
+  const toggleParcelState = () => {
+    if (!direct && !parcel) {
+      setParcel(true);
+    } else if (!parcel && direct) {
+      setParcel(true);
+      setDirect(false);
+    }
+  };
+  const toggleDirectState = () => {
+    if (!direct && !parcel) {
+      setDirect(true);
+    } else if (parcel && !direct) {
+      setParcel(false);
+      setDirect(true);
+    }
+  };
+
+  /* 이미지 관련 */
+  const fileInputRef = useRef(null);
+  const [images, setImages] = useState([]);
+
+  const handleImageClick = () => {
+    if (images.length >= 4) {
+      alert("이미지는 최대 4개까지만 추가할 수 있습니다.");
+      return;
+    }
+    // 이미지 클릭 시 파일 업로드 input 클릭
+    if (fileInputRef.current) {
+      fileInputRef.current.click();
+    }
+  };
+
+  const handleImageChange = (event) => {
+    const selectedImage = event.target.files[0];
+    // 선택된 이미지에 대한 처리 로직 추가
+    const newImage = {
+      id: Date.now(), // 간단한 방법으로 고유한 ID 부여 (실제로는 더 복잡한 방법을 사용해야 함)
+      file: selectedImage,
+    };
+
+    // 기존 이미지 배열에 새 이미지 추가
+    setImages((prevImages) => [...prevImages, newImage]);
+  };
+  /* 이미지 관련 */
   return (
     <div className={styles.container}>
       <div className={styles.header}>
@@ -103,9 +149,25 @@ function Write() {
             <input placeholder="상품에 대해 설명해주세요."></input>
           </div>
           <div className={styles.image}>
-            <div className={styles.eachImage}>
-              <img src={addImage} width="50px" alt="add Image Icon"></img>
+            <div className={styles.eachImage} onClick={handleImageClick}>
+              <img src={addImage} width="70px" alt="add Image Icon"></img>
             </div>
+            <input
+              type="file"
+              accept="image/*"
+              style={{ display: "none" }}
+              ref={fileInputRef}
+              onChange={handleImageChange}
+            />
+            {images.map((image) => (
+              <div key={image.id} className={styles.eachImage}>
+                <img
+                  src={URL.createObjectURL(image.file)}
+                  alt="Uploaded"
+                  width="125px"
+                />
+              </div>
+            ))}
           </div>
           <div>이미지는 최대 4장까지 업로드 할 수 있습니다.</div>
           <div className={styles.category}>
@@ -138,7 +200,22 @@ function Write() {
               사용감 많음
             </div>
           </div>
-          <div className={styles.tradeMethod}>택배거래 / 직거래 선택</div>
+          <div className={styles.tradeMethod}>
+            <div
+              className={`${styles.checkSharing} ${
+                checkSharing && styles.active
+              }`}
+              onClick={toggleCheckSharingState}
+            >
+              택배
+            </div>
+            <div
+              className={`${styles.checkSale} ${checkSale && styles.active}`}
+              onClick={toggleCheckSaleState}
+            >
+              직거래
+            </div>
+          </div>
         </div>
       </div>
     </div>
